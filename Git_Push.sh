c3d38c680c38540ada9f1b4a6b593b760b6e50a5
Jerry_pi <flashlapsedev@gmail.com>
2017-07-19 17:44:52 -0500

    testing autopush

--- Git_Push.sh
+++ Git_Push.sh
@@ -1,3 +1,1 @@
-git push https://github.com/flashlapsedev/Flashlapse_NEO.git
-flashlapsedev@gmail.com
-Guacam0le
+git push https://flashlapsedev@gmail.com:Guacam0le@github.com/flashlapsedev/Flashlapse_NEO.git
