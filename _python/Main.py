3f47e2145593ebfc838dd3549404095cf859f925
Jerry_pi <flashlapsedev@gmail.com>
2017-07-21 12:58:50 -0500

    Method implimentation

--- _python/Main.py
+++ _python/Main.py
@@ -6,6 +6,9 @@ import time
  
 # This gets the Qt stuff
 import PyQt5
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication
+
 import functions
 import Email
 from PyQt5.QtWidgets import *
@@ -14,10 +17,19 @@ directory = None
 import FlashLapse_UI
 import functions
 
+#camera libraries
 from picamera import PiCamera
 from time import sleep
 
-class CaptureImage:  
+#global variables 
+name = None
+directory = None
+interval = None
+duration = None
+snapping = True
+
+class Snapshot:
+
     def __init__(self):
         self._running = True
 
@@ -25,34 +37,87 @@ class CaptureImage:
         self._running = False  
 
     def run(self):
+        global snapping
         while self._running:
-            with PiCamera() as camera:
-                camera.capture("test.jpg")
-            os.system("/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload test.jpg /test")
-            os.system("rm test.jpg")
             
-            Email.sendtest() 
+            with PiCamera() as camera:
+                camera.resolution = (2464,2464)
+                camera._set_rotation(180)
+                camera.capture("../_temp/snapshot.jpg")
+
+            #os.system("/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload test.jpg /test")
+            #os.system("rm test.jpg")
+            
+           # Email.sendtest() 
 
 # create class for our Raspberry Pi GUI
 class MainWindow(QMainWindow, FlashLapse_UI.Ui_MainWindow):
  # access variables inside of the UI's file
 
-   # def IST_Edit(self):
+    def IST_Edit(self):
+        global directory, name
+        name = self.IST_Editor.text()
+        print("name: " + name)
+        
+    def ICI_Change(self):
+        global interval
+        interval = self.ICI_spinBox.value()
+        print(interval)
+        
+    def ISD_Change(self):
+        global duration
+        duration = self.ISD_spinBox.value()
+        print(duration)
+        
+    def Take_Snapshot(self):
+        #self.Snapshot.setEnabled(False)
+        #directory = "/home/pi/Desktop/" + name
+        #self.Directory.setText(directory)
+        
         #file = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
         #Create Class
-      #  Capture = CaptureImage()
+        
+        self.Snapshot.setEnabled(False)
+        self.Snapshot.setText("Processing...")
+        QApplication.processEvents()
+        
+        #Snap = Snapshot()
         #Create Thread
-      #  CaptureThread = Thread(target=Capture.run) 
-      #  #Start Thread 
-      #  CaptureThread.start()
-      #  Capture.terminate()
-     
+        #SnapThread = Thread(target=Snap.run) 
+        #Start Thread
+        
+        #SnapThread.start()
+        #Snap.terminate()
+        
 
     
+       
+
+        
+        with PiCamera() as camera:
+            camera.resolution = (2464,2464)
+            camera._set_rotation(180)
+            camera.capture("../_temp/snapshot.jpg")
+        user_img = PyQt5.QtGui.QImage("../_temp/snapshot.jpg")
+        self.Image_Frame.setPixmap(QtGui.QPixmap(user_img))
+        self.Snapshot.setText("Snapshot")
+        self.Snapshot.setEnabled(True)
+    
+    def Start_Live_Feed(self):
+        
+        with PiCamera() as camera:
+            camera.start_preview()
+            sleep(10)
+            camera.stop_preview()
+
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self) # gets defined in the UI file
         self.IST_Editor.editingFinished.connect(lambda: self.IST_Edit())
+        self.ICI_spinBox.valueChanged.connect(lambda: self.ICI_Change())
+        self.ISD_spinBox.valueChanged.connect(lambda: self.ISD_Change())
+        self.Snapshot.clicked.connect(lambda: self.Take_Snapshot())
+        self.Live_Feed.clicked.connect(lambda: self.Start_Live_Feed)
 
 # I feel better having one of these
 def main():
