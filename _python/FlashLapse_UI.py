137607338ee8dbd0b212401631de4871c1ef989d
Neona <flashlapsedev@gmail.com>
2017-07-24 01:18:23 -0500

    UI Tweak

--- _python/FlashLapse_UI.py
+++ _python/FlashLapse_UI.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 510)
+        MainWindow.resize(800, 503)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("../_image/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
