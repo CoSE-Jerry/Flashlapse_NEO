847ed85ff5e5d180c49b8908cbaa9d52f19afeb8
Neona_XPS <flashlapsedev@gmail.com>
2017-07-30 10:20:42 -0500

    timelapse_added
    time lapse option added, background image recentered

--- _python/FlashLapse_UI.py
+++ _python/FlashLapse_UI.py
@@ -23,7 +23,7 @@ class Ui_MainWindow(object):
         self.Image_Frame.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.Image_Frame.setLineWidth(5)
         self.Image_Frame.setText("")
-        self.Image_Frame.setPixmap(QtGui.QPixmap("../_image/background1.png"))
+        self.Image_Frame.setPixmap(QtGui.QPixmap("../_image/background.png"))
         self.Image_Frame.setScaledContents(True)
         self.Image_Frame.setObjectName("Image_Frame")
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
@@ -279,6 +279,10 @@ class Ui_MainWindow(object):
         self.Storage_Directory.setCheckable(False)
         self.Storage_Directory.setObjectName("Storage_Directory")
         self.verticalLayout_2.addWidget(self.Storage_Directory)
+        self.Timelapse = QtWidgets.QPushButton(self.layoutWidget2)
+        self.Timelapse.setEnabled(True)
+        self.Timelapse.setObjectName("Timelapse")
+        self.verticalLayout_2.addWidget(self.Timelapse)
         self.verticalLayout_3.addLayout(self.verticalLayout_2)
         self.Live_Feed = QtWidgets.QPushButton(self.layoutWidget2)
         self.Live_Feed.setObjectName("Live_Feed")
@@ -425,7 +429,7 @@ class Ui_MainWindow(object):
         self.statusBar.setObjectName("statusBar")
         MainWindow.setStatusBar(self.statusBar)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 19))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.actionOpen_Directory = QtWidgets.QAction(MainWindow)
@@ -471,8 +475,8 @@ class Ui_MainWindow(object):
         self.Gravi_Text.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">12 hours full spectrum light from above, 90 degree rotation &amp; all lights become a certain color.</p></body></html>"))
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.1pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8.25pt;\">12 hours full spectrum light from above, 90 degree rotation &amp; all lights become a certain color.</span></p></body></html>"))
         self.Gravi_Confirm.setText(_translate("MainWindow", "Confirm "))
         self.Color_Gravi.setText(_translate("MainWindow", "Rotation Color:"))
         self.Gravi_Red.setText(_translate("MainWindow", "Red"))
@@ -483,8 +487,8 @@ class Ui_MainWindow(object):
         self.Germi_Text.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">7 days of a certain colored light from all sides.</p></body></html>"))
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.1pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8.25pt;\">7 days of a certain colored light from all sides.</span></p></body></html>"))
         self.Germi_Confirm.setText(_translate("MainWindow", "Confirm "))
         self.Color_Germi.setText(_translate("MainWindow", "Color Setting:"))
         self.Germi_Red.setText(_translate("MainWindow", "Red"))
@@ -495,8 +499,8 @@ class Ui_MainWindow(object):
         self.Barrier_Text.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">24 hour of a certain colored light from above.</p></body></html>"))
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.1pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8.25pt;\">24 hour of a certain colored light from above.</span></p></body></html>"))
         self.Barrier_Confirm.setText(_translate("MainWindow", "Confirm "))
         self.Color_Barri.setText(_translate("MainWindow", "Color Setting:"))
         self.Barri_Red.setText(_translate("MainWindow", "Red"))
@@ -515,6 +519,7 @@ class Ui_MainWindow(object):
         self.ISD_spinBox.setSuffix(_translate("MainWindow", " min"))
         self.Directory_Label.setText(_translate("MainWindow", "Storage Directory Not Selected"))
         self.Storage_Directory.setText(_translate("MainWindow", "Select Storage Directory"))
+        self.Timelapse.setText(_translate("MainWindow", "Timelapse Generation: ON"))
         self.Live_Feed.setText(_translate("MainWindow", "Start Live Feed (30s)"))
         self.Progress_Label.setText(_translate("MainWindow", "Progress:"))
         self.Control_Tab.setTabText(self.Control_Tab.indexOf(self.Imaging), _translate("MainWindow", "Imaging"))
