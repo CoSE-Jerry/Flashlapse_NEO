d2a9e48dfa7382a7a044b0e4b2eb32a592e3e019
Jerry Miao <flashlapsedev@gmail.com>
2018-04-23 12:11:08 -0500

    bug fix

--- _python/Main.py
+++ _python/Main.py
@@ -569,7 +569,7 @@ class MainWindow(QMainWindow, FlashLapse_UI.Ui_MainWindow):
 
     def value_changed(self):
     
-        self.Motor_Speed.setText("Motor Speed: "+str(Motor_Speed.value()))
+        self.Motor_Speed.setText("Motor Speed: "+str(Speed_Slider.value()))
         
 
         
