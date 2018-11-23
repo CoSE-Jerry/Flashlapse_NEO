12c6ac9b29e37597e2e6e3d7e223e8d46f0329c3
Jerry Miao <flashlapsedev@gmail.com>
2018-11-23 09:09:03 -0500

    function update

--- _python/UI_Update_Disable.py
+++ _python/UI_Update_Disable.py
@@ -1,6 +1,7 @@
 #disable all ui elements that utilizes the cameraduring the snapshot is taken
 def snap_disable(self):
     self.Snapshot.setEnabled(False)
+    self.Snapshot.setText("Processing...")
     self.Live_Feed.setEnabled(False)
     if self.Start_Imaging.isEnabled() == True:
         self.Start_Imaging.setEnabled(False)
--- _python/UI_Update_Enable.py
+++ _python/UI_Update_Enable.py
@@ -1,5 +1,8 @@
 #disable all ui elements that utilizes the cameraduring the snapshot is taken
 def snap_enable(self):
+    snap_img = PyQt5.QtGui.QImage("../_temp/snapshot.jpg")
+    self.Image_Frame.setPixmap(QtGui.QPixmap(snap_img))
+    self.Snapshot.setText("Snapshot")
     self.Snapshot.setEnabled(True)
     self.Live_Feed.setEnabled(True)
     if not self.Start_Imaging.isEnabled():
