b5ed2e8994213f1fb3a843cd287dc8b9e49bff82
Jerry Miao <flashlapsedev@gmail.com>
2018-11-23 12:49:51 -0500

    update

--- _python/UI_Update_General.py
+++ _python/UI_Update_General.py
@@ -1,14 +1,8 @@
 def check_stat(self):
-    return self.Start_Imaging.isEnabled(),self.Start_Schedule.isEnabled()
-
-def start_image_disable(self):
-    self.Start_Imaging.setEnabled(False)
+    return self.Start_Schedule.isEnabled()
 
 def schedule_disable(self):
     self.Start_Schedule.setEnabled(False)
 
-def start_image_enable(self):
-    self.Start_Imaging.setEnabled(True)
-
-def start_image_enable(self):
+def schedule_enable(self):
     self.Start_Schedule.setEnabled(True)
