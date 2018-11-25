2f6d07d2c8bfb518f44163404ae7349bd3658402
Jerry Miao <flashlapsedev@gmail.com>
2018-11-24 21:54:14 -0500

    Update Temp.py

--- _python/Temp.py
+++ _python/Temp.py
@@ -179,7 +179,7 @@ class MainWindow(QMainWindow, FlashLapse_UI.Ui_MainWindow):
         self.Storage_Directory.clicked.connect(lambda: self.Select_Storage_Directory())
         self.Start_Schedule.clicked.connect(lambda: self.start_scheduler())
         self.Test_Run.clicked.connect(lambda: self.start_scheduler())
-        self.Reset_Position.clicked.connect(lambda: self.start_scheduler())
+        self.Reset_Position.clicked.connect(lambda: self.reset_position())
         
         
 
