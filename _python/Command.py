074c78f85372a1ee67bd311e779dbccf55795ccb
Jerry Miao <flashlapsedev@gmail.com>
2018-11-25 12:31:10 -0500

    update

--- _python/Command.py
+++ _python/Command.py
@@ -16,10 +16,10 @@ class Schedule(QThread):
         while True:
             Settings.ASD.write(bytes('~'+str(Settings.angle_1)+"\n", 'UTF-8'))
             Settings.ASD.write(bytes('~0'+"\n", 'UTF-8'))
-            sleep(Settings.delay_1*60)
+            sleep(Settings.delay_1*5)
             Settings.ASD.write(bytes('~'+str(Settings.angle_2)+"\n", 'UTF-8'))
             Settings.ASD.write(bytes('~0'+"\n", 'UTF-8'))
-            sleep(Settings.delay_2*60)
+            sleep(Settings.delay_2*5)
 
 class Test(QThread):
     
