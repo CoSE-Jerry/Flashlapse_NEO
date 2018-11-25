d994c368fd6b40f0a7c8b074a464e558254e1e12
Jerry Miao <flashlapsedev@gmail.com>
2018-11-25 12:46:25 -0500

    update

--- _python/Command.py
+++ _python/Command.py
@@ -15,15 +15,17 @@ class Schedule(QThread):
     def run(self):
         while True:
             Settings.ASD.write(bytes('~'+str(Settings.angle_1)+"\n", 'UTF-8'))
-            sleep(Settings.delay_1*60)
+            Settings.ASD.write(bytes('~0'+"\n", 'UTF-8'))
+            sleep(Settings.delay_1*5)
             Settings.ASD.write(bytes('~'+str(Settings.angle_2)+"\n", 'UTF-8'))
-            sleep(Settings.delay_2*60)
+            Settings.ASD.write(bytes('~0'+"\n", 'UTF-8'))
+            sleep(Settings.delay_2*5)
 
 class Test(QThread):
     
     def __init__(self):
         QThread.__init__(self)
-        Settings.Test_running = True
+        Settings.sch_running = True
 
     def __del__(self):
         self._running = False
@@ -31,6 +33,8 @@ class Test(QThread):
     def run(self):
         while True:
             Settings.ASD.write(bytes('~'+str(Settings.angle_1)+"\n", 'UTF-8'))
+            Settings.ASD.write(bytes("~0\n", "UTF-8"))
             sleep(5)
             Settings.ASD.write(bytes('~'+str(Settings.angle_2)+"\n", 'UTF-8'))
+            Settings.ASD.write(bytes("~0\n", "UTF-8"))
             sleep(5)
