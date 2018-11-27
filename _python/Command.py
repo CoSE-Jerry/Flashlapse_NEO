faf98dd427419ef9efe263560f761dc495cfa760
Jerry Miao <flashlapsedev@gmail.com>
2018-11-27 11:15:01 -0500

    update

--- _python/Command.py
+++ _python/Command.py
@@ -77,3 +77,42 @@ def half_color_change_right(self):
         Settings.ASD.write(bytes('D', 'UTF-8'))
     elif temp == 0:
         Settings.ASD.write(bytes('E', 'UTF-8'))
+
+def third_color_change_top(self):
+    temp = self.Left_Select.currentIndex()
+    if temp == 1:
+        Settings.ASD.write(bytes('f', 'UTF-8'))
+    elif temp == 2:
+        Settings.ASD.write(bytes('g', 'UTF-8'))
+    elif temp == 3:
+        Settings.ASD.write(bytes('h', 'UTF-8'))
+    elif temp == 4:
+        Settings.ASD.write(bytes('i', 'UTF-8'))
+    else:
+        Settings.ASD.write(bytes('j', 'UTF-8'))
+
+def third_color_change_lower_left(self):
+    temp = self.Left_Select.currentIndex()
+    if temp == 1:
+        Settings.ASD.write(bytes('k', 'UTF-8'))
+    elif temp == 2:
+        Settings.ASD.write(bytes('l', 'UTF-8'))
+    elif temp == 3:
+        Settings.ASD.write(bytes('m', 'UTF-8'))
+    elif temp == 4:
+        Settings.ASD.write(bytes('n', 'UTF-8'))
+    else:
+        Settings.ASD.write(bytes('o', 'UTF-8'))
+
+def third_color_change_lower_right(self):
+    temp = self.Left_Select.currentIndex()
+    if temp == 1:
+        Settings.ASD.write(bytes('p', 'UTF-8'))
+    elif temp == 2:
+        Settings.ASD.write(bytes('q', 'UTF-8'))
+    elif temp == 3:
+        Settings.ASD.write(bytes('r', 'UTF-8'))
+    elif temp == 4:
+        Settings.ASD.write(bytes('s', 'UTF-8'))
+    else:
+        Settings.ASD.write(bytes('t', 'UTF-8'))
