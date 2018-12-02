c256bae3a7f57d15153bfdab6e71e8e45d68633a
Jerry Miao <jerry@coseinstruments.com>
2018-12-02 15:10:33 -0600

    update

--- _python/Command.py
+++ _python/Command.py
@@ -5,12 +5,11 @@ from PyQt5.QtCore import QThread
 def full_color_change(self):
     temp = self.Full_Color_Select.currentIndex()
 
-    if(self.Left_Select.currentIndex() != 0):
-        self.Left_Select.setCurrentIndex(0)
-    #self.Right_Select.setCurrentIndex(0)
-    #self.Top_Color_Select.setCurrentIndex(0)
-    #self.LL_Color_Select.setCurrentIndex(0)
-    #self.LR_Color_Select.setCurrentIndex(0)
+    self.Left_Select.setCurrentIndex(0)
+    self.Right_Select.setCurrentIndex(0)
+    self.Top_Color_Select.setCurrentIndex(0)
+    self.LL_Color_Select.setCurrentIndex(0)
+    self.LR_Color_Select.setCurrentIndex(0)
 
     if temp == 1:
         Settings.ASD.write(bytes("1~0~58~255~0~0~1", 'UTF-8'))
@@ -29,23 +28,23 @@ def half_color_change_left(self):
 
     temp = self.Left_Select.currentIndex()
 
-    if(self.Full_Color_Select.currentIndex() != 0):
-        self.Full_Color_Select.setCurrentIndex(0)
-        
-    #self.Top_Color_Select.setCurrentIndex(0)
-    #self.LL_Color_Select.setCurrentIndex(0)
-    #self.LR_Color_Select.setCurrentIndex(0)
+    self.Full_Color_Select.setCurrentIndex(0)    
+    self.Top_Color_Select.setCurrentIndex(0)
+    self.LL_Color_Select.setCurrentIndex(0)
+    self.LR_Color_Select.setCurrentIndex(0)
 
     if temp == 1:
-        Settings.ASD.write(bytes('1~0~29~255~0~0~2', 'UTF-8'))
+        Settings.ASD.write(bytes("1~0~29~255~0~0~2", 'UTF-8'))
     elif temp == 2:
-        Settings.ASD.write(bytes('B', 'UTF-8'))
+        Settings.ASD.write(bytes("1~0~29~0~255~0~2", 'UTF-8'))
     elif temp == 3:
-        Settings.ASD.write(bytes('C', 'UTF-8'))
+        Settings.ASD.write(bytes("1~0~29~0~0~255~2", 'UTF-8'))
     elif temp == 4:
-        Settings.ASD.write(bytes('D', 'UTF-8'))
+        Settings.ASD.write(bytes("1~0~29~85~85~85~2", 'UTF-8'))
+    elif temp == 5:
+        Settings.ASD.write(bytes("1~0~29~" + str(Settings.custom_R) + "~" + str(Settings.custom_G) + "~" + str(Settings.custom_B) + "~2", 'UTF-8'))
     else:
-        Settings.ASD.write(bytes('E', 'UTF-8'))
+        Settings.ASD.write(bytes("1~0~29~0~0~0~2", 'UTF-8'), 'UTF-8'))
 
 def half_color_change_right(self):
 
@@ -56,15 +55,17 @@ def half_color_change_right(self):
     
     temp = self.Right_Select.currentIndex()
     if temp == 1:
-        Settings.ASD.write(bytes('a', 'UTF-8'))
+        Settings.ASD.write(bytes("1~29~58~255~0~0~2", 'UTF-8'))
     elif temp == 2:
-        Settings.ASD.write(bytes('b', 'UTF-8'))
+        Settings.ASD.write(bytes("1~29~58~0~255~0~2", 'UTF-8'))
     elif temp == 3:
-        Settings.ASD.write(bytes('c', 'UTF-8'))
+        Settings.ASD.write(bytes("1~29~58~0~0~255~2", 'UTF-8'))
     elif temp == 4:
-        Settings.ASD.write(bytes('d', 'UTF-8'))
-    elif temp == 0:
-        Settings.ASD.write(bytes('e', 'UTF-8'))
+        Settings.ASD.write(bytes("1~29~58~85~85~85~2", 'UTF-8'))
+    elif temp == 5:
+        Settings.ASD.write(bytes("1~29~58~" + str(Settings.custom_R) + "~" + str(Settings.custom_G) + "~" + str(Settings.custom_B) + "~2", 'UTF-8'))
+    else temp == 0:
+        Settings.ASD.write(bytes("1~29~58~0~0~0~2", 'UTF-8'))
 
 def third_color_change_top(self):
 
@@ -74,15 +75,17 @@ def third_color_change_top(self):
     
     temp = self.Top_Color_Select.currentIndex()
     if temp == 1:
-        Settings.ASD.write(bytes('f', 'UTF-8'))
+        Settings.ASD.write(bytes("1~0~19~255~0~0~3", 'UTF-8'))
     elif temp == 2:
-        Settings.ASD.write(bytes('g', 'UTF-8'))
+        Settings.ASD.write(bytes("1~0~19~0~255~0~3", 'UTF-8'))
     elif temp == 3:
-        Settings.ASD.write(bytes('h', 'UTF-8'))
+        Settings.ASD.write(bytes("1~0~19~0~0~255~3", 'UTF-8'))
     elif temp == 4:
-        Settings.ASD.write(bytes('i', 'UTF-8'))
+        Settings.ASD.write(bytes("1~0~19~85~85~85~3", 'UTF-8'))
+    elif temp == 5:
+        Settings.ASD.write(bytes("1~0~19~" + str(Settings.custom_R) + "~" + str(Settings.custom_G) + "~" + str(Settings.custom_B) + "~3", 'UTF-8'))
     else:
-        Settings.ASD.write(bytes('j', 'UTF-8'))
+        Settings.ASD.write(bytes("1~0~19~0~0~0~3", 'UTF-8'))
 
 def third_color_change_lower_left(self):
 
@@ -92,15 +95,17 @@ def third_color_change_lower_left(self):
     
     temp = self.LL_Color_Select.currentIndex()
     if temp == 1:
-        Settings.ASD.write(bytes('k', 'UTF-8'))
+        Settings.ASD.write(bytes("1~19~38~255~0~0~3", 'UTF-8'))
     elif temp == 2:
-        Settings.ASD.write(bytes('l', 'UTF-8'))
+        Settings.ASD.write(bytes("1~19~38~0~255~0~3", 'UTF-8'))
     elif temp == 3:
-        Settings.ASD.write(bytes('m', 'UTF-8'))
+        Settings.ASD.write(bytes("1~19~38~0~0~255~3", 'UTF-8'))
     elif temp == 4:
-        Settings.ASD.write(bytes('n', 'UTF-8'))
+        Settings.ASD.write(bytes("1~19~38~85~85~85~3", 'UTF-8'))
+    elif temp == 5:
+        Settings.ASD.write(bytes("1~19~38~" + str(Settings.custom_R) + "~" + str(Settings.custom_G) + "~" + str(Settings.custom_B) + "~3", 'UTF-8'))
     else:
-        Settings.ASD.write(bytes('o', 'UTF-8'))
+        Settings.ASD.write(bytes("1~19~38~255~0~0~3", 'UTF-8'))
 
 def third_color_change_lower_right(self):
 
@@ -110,12 +115,14 @@ def third_color_change_lower_right(self):
     
     temp = self.LR_Color_Select.currentIndex()
     if temp == 1:
-        Settings.ASD.write(bytes('p', 'UTF-8'))
+        Settings.ASD.write(bytes("1~38~58~255~0~0~3", 'UTF-8'))
     elif temp == 2:
-        Settings.ASD.write(bytes('q', 'UTF-8'))
+        Settings.ASD.write(bytes("1~38~58~255~0~0~3", 'UTF-8'))
     elif temp == 3:
-        Settings.ASD.write(bytes('r', 'UTF-8'))
+        Settings.ASD.write(bytes("1~38~58~255~0~0~3", 'UTF-8'))
     elif temp == 4:
-        Settings.ASD.write(bytes('s', 'UTF-8'))
+        Settings.ASD.write(bytes("1~38~58~255~0~0~3", 'UTF-8'))
+    elif temp == 5:
+        Settings.ASD.write(bytes("1~38~58~" + str(Settings.custom_R) + "~" + str(Settings.custom_G) + "~" + str(Settings.custom_B) + "~3", 'UTF-8'))
     else:
-        Settings.ASD.write(bytes('t', 'UTF-8'))
+        Settings.ASD.write(bytes("1~38~58~0~0~0~3", 'UTF-8'))
