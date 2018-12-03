43befe97fbf456f36cced4c26a8e0108b77cbb82
Jerry Miao <jerry@coseinstruments.com>
2018-12-02 20:52:32 -0600

    update

--- _python/UI_Update_General.py
+++ _python/UI_Update_General.py
@@ -1,4 +1,7 @@
 import Settings
+import PyQt5
+
+from PyQt5 import QtCore, QtGui, QtWidgets
 def check_stat(self):
     return self.Start_Schedule.isEnabled()
 
@@ -10,8 +13,23 @@ def schedule_enable(self):
 
 def schedule_update(self):
     sn = len(Settings.sequence_name)
-    em = len(Settings.email)
-    if(sn != 0 and em != 0 and Settings.sch_confirmed):
+    if(sn != 0 and Settings.sch_confirmed):
         schedule_enable(self)
     else:
         schedule_disable(self)
+
+def snap_disable(self,sch_flip):
+    self.Snapshot.setEnabled(False)
+    self.Snapshot.setText("Processing...")
+    self.Live_Feed.setEnabled(False)
+    if sch_flip:
+        self.Start_Schedule.setEnabled(False)
+
+def snap_enable(self,sch_flip):
+    snap_img = PyQt5.QtGui.QImage("../_temp/snapshot.jpg")
+    self.Image_Frame.setPixmap(QtGui.QPixmap(snap_img))
+    self.Snapshot.setText("Snapshot")
+    self.Snapshot.setEnabled(True)
+    self.Live_Feed.setEnabled(True)
+    if sch_flip:
+        self.Start_Schedule.setEnabled(True)
