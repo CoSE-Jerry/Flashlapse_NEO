04a165fb326cd064a4ce5c2e248a09243ce0e19a
Jerry Miao <jerry@coseinstruments.com>
2019-06-22 17:53:55 -0500

    Update Settings.py

--- _python/Settings.py
+++ _python/Settings.py
@@ -2,7 +2,7 @@ import serial
 
 def init():
     global ASD
-    ASD = serial.Serial('/dev/ttyACM0', 9600)
+    ASD = serial.Serial('/dev/ttyAMA0', 9600)
 
     global commands_list
     commands_list = []
@@ -12,7 +12,7 @@ def init():
 
 
 def sendCMD():
-    serial.Serial('/dev/ttyACM0', 9600).write(bytes(current_CMD, 'UTF-8'))
+    serial.Serial('/dev/ttyAMA0', 9600).write(bytes(current_CMD, 'UTF-8'))
     
 
     '''global sch_running
