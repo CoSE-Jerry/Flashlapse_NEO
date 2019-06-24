3b44c4f730b704d12cf3d2e3cf05dc81a889ef22
Jerry@Starting_block <jerry@coseinstruments.com>
2019-06-24 02:42:16 -0500

    Update Main.py

--- _python/Main.py
+++ _python/Main.py
@@ -34,18 +34,18 @@ date = time.strftime('%m_%d_%Y')
 class MainWindow(QMainWindow, FlashLapse_UI.Ui_MainWindow):
 
     def start_cycle(self):
-    if not Settings.cycle_running:
-        try:
-            Settings.cycle_time = self.powerCycle_spinBox.value()
-            self.Cycle_Thread = Threads.Cycle()
-            self.Cycle_Thread.started.connect(lambda: UI_Update.cycle_start(self))
-            self.Cycle_Thread.finished.connect(lambda: UI_Update.cycle_end(self))
-            self.Cycle_Thread.start()
-            
-        except Exception as e:
-            print(e)
-    else:
-        Settings.cycle_running = False
+        if not Settings.cycle_running:
+            try:
+                Settings.cycle_time = self.powerCycle_spinBox.value()
+                self.Cycle_Thread = Threads.Cycle()
+                self.Cycle_Thread.started.connect(lambda: UI_Update.cycle_start(self))
+                self.Cycle_Thread.finished.connect(lambda: UI_Update.cycle_end(self))
+                self.Cycle_Thread.start()
+                
+            except Exception as e:
+                print(e)
+        else:
+            Settings.cycle_running = False
 
     '''def Start_Snapshot(self):
         try:
