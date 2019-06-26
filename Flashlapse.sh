030c7ac3a2f8088c42f505a6ba67cb9c80c3764c
Jerry Miao <jerry@coseinstruments.com>
2019-06-25 21:10:00 -0500

    Update Flashlapse.sh

--- Flashlapse.sh
+++ Flashlapse.sh
@@ -1,4 +1,7 @@
-cd /home/pi/Flashlapse_NEO
-git pull
+if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+	cd /home/pi/Flashlapse_NEO
+	git pull
+fi
+
 cd /home/pi/Flashlapse_NEO/_python
 sudo python3 Main.py
